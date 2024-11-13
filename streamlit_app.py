6b738ec9d2712622b1f7c433bceaa345e7b7a9f6
ghfjdksla123 <150836647+ghfjdksla123@users.noreply.github.com>
2024-11-13 10:53:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,6 @@ st.write("""Choose the fruits you want in your custom Smoothie!""")
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect(
@@ -39,7 +36,8 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        cnx = st.connection("snowflake")
+        session = cnx.session()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
     
